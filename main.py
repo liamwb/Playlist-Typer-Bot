a8c1062653d9b84fdc8522df5778d35bb20893d2
Liam's Laptop <liam.woodbaker@gmail.com>
2020-04-09 15:41:45 +1000

    Some Comments

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+"""
+Playlist-Typer-Bot, by Liam Wood-Baker, 2020.
+This bot listens for someone who wants to queue a spotify playlist in a chat with Rhythm in it, and
+writes the commands for each song in the playlist in to the chat.
+"""
+
+
 import spotipy.oauth2 as oauth2
 import spotipy
+import json
 import discord
 from discord.ext import commands
 
+# these are secret tokens, which you will need to create in the same location as this file
 discordToken = open('DISCORD-TOKEN.txt').read()
-
-
 clientSecret = open('SPOTIFY-CLIENT-SECRET.txt').read()
 
+# getting authorisation for spotify
 credentials = oauth2.SpotifyClientCredentials(
         client_id='b9a5c48552a84a5fb7a47acea0d2d9fc',
         client_secret=clientSecret)
@@ -15,12 +23,12 @@ credentials = oauth2.SpotifyClientCredentials(
 spotifyToken = credentials.get_access_token()
 spotify = spotipy.Spotify(auth=spotifyToken)
 
-print(spotify.playlist(playlist_id='https://open.spotify.com/playlist/5KfPRv6ynvPD2VwfD4y7ni?si=f6B4-7FURgSlbr-hSZTT7g'))
 
+playlistTest = spotify.playlist(playlist_id='https://open.spotify.com/playlist/5KfPRv6ynvPD2VwfD4y7ni?si=f6B4-7FURgSlbr-hSZTT7g')
+print(json.dumps(playlistTest, indent=4))
 
-
+# the bot proper begins. Authorisation for the discord bot happens at the end with bot.run(discordToken)
 bot = commands.Bot(command_prefix='!')
-
 
 @bot.event
 async def on_ready():
